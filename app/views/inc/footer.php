5a5431e80850eb1c5229b420080a557558c6cd8f
Saja Saadoun <saja1904826@miuegypt.edu.eg>
2022-04-03 04:54:21 +0200

    edit2

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -2,5 +2,43 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
   <script src="<?php echo URLROOT; ?>js/main.js"></script>
   </body>
-
+  <footer class="u-align-left u-clearfix u-custom-color-3 u-footer u-footer" id="sec-b5e2"><h2 class="u-subtitle u-text u-text-1">Contact ​Us</h2><h1 class="u-text u-text-default u-title u-text-2"> Faculties Links </h1><p class="u-text u-text-3">KM 28 Cairo – Ismailia Road Ahmed Orabi District, Cairo – Egypt<i></i>MIU&nbsp;<br>Email: miu@miuegypt.edu.eg<i></i>MIU&nbsp;<br>E-Learning Email: e-learning@miuegypt.edu.eg<i></i>
+        <br>MIU Hotline: 19-MIU / 19648<br>
+      </p><ul class="u-text u-text-default u-text-4">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-1" href="https://www.miuegypt.edu.eg/academics/faculty-of-alsun/" target="_blank">
+          <li>Alsun</li>
+        </a>
+      </ul><ul class="u-text u-text-default u-text-5">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-white u-btn-2" href="https://www.miuegypt.edu.eg/academics/faculty-of-oral/" target="_blank">
+          <li> Oral Dental Medicine</li>
+        </a>
+      </ul><ul class="u-text u-text-default u-text-6">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-3" href="https://www.miuegypt.edu.eg/academics/faculty-of-computer-science-2/" target="_blank">
+          <li>Computer Science</li>
+        </a>
+      </ul><ul class="u-text u-text-default u-text-7">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-white u-btn-4" href="https://www.miuegypt.edu.eg/academics/faculty-of-oral/ -----------------------" target="_blank">
+          <li>Pharmacy</li>
+        </a>
+      </ul><ul class="u-text u-text-default u-text-8">
+        <li>
+          <a href="https://www.miuegypt.edu.eg/academics/faculty-of-mass-communication/" class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-5" target="_blank">Mass Comunicatio</a>n
+        </li>
+      </ul><ul class="u-text u-text-default u-text-9">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-6" href="https://www.miuegypt.edu.eg/academics/faculty-of-architecture/" target="_blank">
+          <li> Engineering - Architecture</li>
+        </a>
+      </ul><ul class="u-text u-text-default u-text-10">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-7" href="https://www.miuegypt.edu.eg/academics/faculty-of-business-2/" target="_blank">
+          <li>Bussiness Adminstration</li>
+        </a>
+      </ul><ul class="u-text u-text-default u-text-11">
+        <a class="u-active-none u-border-none u-btn u-button-link u-button-style u-hover-none u-none u-text-white u-btn-8" href="https://www.miuegypt.edu.eg/academics/faculty-of-electronics/" target="_blank">
+          <li> Engineering - Electronics</li>
+        </a>
+      </ul><div class="u-container-style u-custom-color-4 u-expanded-width u-group u-shape-rectangle u-group-1">
+        <div class="u-container-layout u-valign-middle u-container-layout-1">
+          <p class="u-text u-text-custom-color-5 u-text-default u-text-12"> Copyright © MIU 2021. All rights reserved.</p>
+        </div>
+      </div></footer>
   </html>
