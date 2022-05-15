acd9cb20475514e50c4099649313c3f6ad10fcc6
farahkhaled1 <farah1900784@miuegypt.edu.eg>
2022-05-15 19:52:56 +0200

    login front end

---
+++ resources/views/login.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login</title>
+    <!-- plugins:css
+    <link rel="stylesheet" href="../../vendors/feather/feather.css" />
+    <link rel="stylesheet" href="../../vendors/mdi/css/materialdesignicons.min.css" />
+    <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css" />
+    <link rel="stylesheet" href="../../vendors/typicons/typicons.css" />
+    <link rel="stylesheet" href="../../vendors/simple-line-icons/css/simple-line-icons.css" />
+    <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css" /> -->
+
+    <link rel="stylesheet" href="../css/vertical-layout-light/style.css" />
+    <link rel="shortcut icon" href="../../images/favicon.png" />
+    <link rel="stylesheet" href="../css/nicepage.css" media="screen">
+    <link rel="stylesheet" href="../css/Home.css" media="screen">
+    <script class="u-script" type="text/javascript" src="../js/jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="../js/nicepage.js" defer=""></script>
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=PT+Sans:400,400i,700,700i">
+
+
+</head>
+
+<body>
+    <header class=" u-header u-section-row-container" id="sec-f4f6" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">
+        <div class="u-section-rows">
+            <div class="u-gradient u-section-row u-section-row-1" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="" id="sec-5f8d">
+                <div class="u-clearfix u-sheet u-sheet-1">
+                    <a href="" class="u-image u-logo u-image-1" data-image-width="654" data-image-height="194">
+                        <img src="../../public/images/logo.png" class="u-logo-image u-logo-image-1">
+                    </a>
+
+                </div>
+
+
+
+
+
+            </div>
+        </div>
+        </div>
+        </div>
+    </header>
+    <div class="container-scroller">
+        <div class="container-fluid page-body-wrapper full-page-wrapper">
+            <div class="content-wrapper d-flex align-items-center auth px-0">
+                <div class="row w-100 mx-0">
+                    <div class="col-lg-4 mx-auto">
+                        <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                            <div class="brand-logo">
+                                <img src="../../public/images/logo.png" alt="logo" />
+                            </div>
+                            <h4>Hello! let's get started</h4>
+                            <h6 class="fw-light">Sign in to continue.</h6>
+                            <form class="pt-3">
+                                <div class="form-group">
+                                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" />
+                                </div>
+                                <div class="mt-3">
+                                    <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="home.blade.php">SIGN IN</a>
+                                </div>
+                                <div class="my-2 d-flex justify-content-between align-items-center">
+
+                                    <a href="#" class="auth-link text-black">Forgot password?</a>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- content-wrapper ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="../../vendors/js/vendor.bundle.base.js"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <script src="../../vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="../../js/off-canvas.js"></script>
+    <script src="../../js/hoverable-collapse.js"></script>
+    <script src="../../js/template.js"></script>
+    <script src="../../js/settings.js"></script>
+    <script src="../../js/todolist.js"></script>
+    <!-- endinject -->
+</body>
+
+<footer class="u-align-center u-clearfix u-footer u-grey-80" id="sec-9d69">
+    <div class="u-clearfix u-sheet u-sheet-1">
+        <p style=margin-left:20px; class="u-align-left u-medium-text u-text u-text-variant u-text-1">Copyright © MIU 2021. All rights reserved.<br>
+        </p>
+        <h5 class="u-align-left u-text u-text-2"></h5>
+    </div>
+</footer>
+
+</html>
